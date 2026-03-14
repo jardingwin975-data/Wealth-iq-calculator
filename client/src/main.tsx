56652b4282cf92551a643cff1ea2c54825bed1d5
Jardin Gwin <jardingwin975@gmail.com>
2026-03-13 22:32:34 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  createRoot(rootElement).render(<App />);
-}
+createRoot(document.getElementById("root")!).render(
+  <App />
+);
