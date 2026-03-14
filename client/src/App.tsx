b1f92149b502d2c6a599dc89fdcbf6008ca181a7
Jardin Gwin <jardingwin975@gmail.com>
2026-03-14 12:32:29 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,8 @@
 import Home from "./pages/Home";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
+import NotFound from "./pages/not-found";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
 export default function App() {
   return <Home />;
