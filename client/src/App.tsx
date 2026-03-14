bdee0cfdc6b96be9eb6c6fbded5dfd30e8359b4c
Jardin Gwin <jardingwin975@gmail.com>
2026-03-14 13:15:38 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,5 @@
+import Home from "./pages/Home";
+
 export default function App() {
-  return (
-    <div style={{ padding: 40, background: "white", minHeight: "100vh", color: "#111" }}>
-      <h1>App mounted</h1>
-      <p>If you see this, App.tsx is working.</p>
-    </div>
-  );
+  return <Home />;
 }
