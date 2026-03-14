37cbf490edbcd4da1122b960b52c846160d571d3
Jardin Gwin <jardingwin975@gmail.com>
2026-03-13 21:38:26 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <div style={{ padding: 40, color: "black", background: "white" }}>
-    <h1>MAIN TSX DIRECT TEST</h1>
-    <p>If you see this, the JS bundle is loading and React is mounting.</p>
-  </div>
-);
+createRoot(document.getElementById("root")!).render(<App />);
