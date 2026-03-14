87551175cf68caafdc426db54476c879496133cc
Jardin Gwin <jardingwin975@gmail.com>
2026-03-13 22:41:36 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import Home from "./pages/Home";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <App />
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
 );
