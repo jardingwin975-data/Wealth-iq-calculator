231cae0588bd701c214f81005216ac04952cba96
Jardin Gwin <jardingwin975@gmail.com>
2026-03-14 19:07:08 -0500

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,11 @@
-import tailwindcssAnimate from "tailwindcss-animate";
-import typography from "@tailwindcss/typography";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
   content: [
-    "./client/index.html",
-    "./client/src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./client/src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [tailwindcssAnimate, typography],
-};
+  plugins: [],
+}
